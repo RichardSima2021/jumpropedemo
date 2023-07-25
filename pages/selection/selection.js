41c46f739281e822ef987220e5e02e9e77a1906d
richardsima <biscuitsima@tencent.com>
2023-07-25 16:42:45 +0800

    changed test event name

--- pages/selection/selection.js
+++ pages/selection/selection.js
@@ -13,7 +13,7 @@ Page({
             {"eventName":"event1","date":"1start-1end"},
             {"eventName":"event2","date":"2start-2end"},
             {"eventName":"event3","date":"3start-3end"},
-            {"eventName":"event3","date":"4start-4end"}
+            {"eventName":"event4","date":"4start-4end"}
         ],
         text: "Hello",
         scrollDist:String,
