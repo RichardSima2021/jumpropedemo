b7135a6d41060013f386b0f9d254b81c14ea646f
richardsima <biscuitsima@tencent.com>
2023-07-25 19:48:30 +0800

    cleaned up wxml

--- pages/selection/selection.js
+++ pages/selection/selection.js
@@ -9,16 +9,24 @@ function navigatePage(page){
 
 Page({
     data:{
-        events:[
+        fakeDB:[
             {"eventName":"event1","date":"1start-1end"},
             {"eventName":"event2","date":"2start-2end"},
             {"eventName":"event3","date":"3start-3end"},
-            {"eventName":"event4","date":"4start-4end"}
+            {"eventName":"event4","date":"4start-4end"},
+            {"eventName":"event5","date":"5start-5end"},
+            {"eventName":"event6","date":"6start-6end"},
+            {"eventName":"event7","date":"7start-7end"},
+            {"eventName":"event8","date":"8start-8end"},
+            {"eventName":"event9","date":"9start-9end"}
         ],
         text: "Hello",
         scrollDist:String,
         enable: false,
-        loadingProps:{size:'50rpx'}
+        loadingProps:{size:'50rpx'},
+        noMoreContent: false,
+        defaultEventListSize: 4,
+        currentEventListSize: 4
     },
     onTabsChange(e){
         navigatePage(e.detail.value);
@@ -40,6 +48,12 @@ Page({
     },
     onReachBottom(){
         console.log("bottom");
+        // console.log(Object.keys(this.data.fakeDB).length)
+        if(this.data.currentEventListSize < Object.keys(this.data.fakeDB).length){
+            console.log("no more data");
+            this.data.currentEventListSize += this.data.defaultEventListSize;
+            this.data.noMoreContent = (this.data.currentEventListSize >= this.data.defaultEventListSize);
+        }
     }
 }
 
